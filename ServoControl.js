360fb95bebe36ba464900081aec0b07d6505bbe0
Jason Kridner <jdk@ti.com>
2015-07-20 22:23:48 -0400

    Add axis and fix duty_min

--- ServoControl.js
+++ ServoControl.js
@@ -1,10 +1,12 @@
 var b = require('bonescript');
 var xSERVO = 'P9_14';
+var ySERVO = 'P9_22';
 
 //Servo info
 var increment = 0.01;
 var xposition = 0.55;
 var yposition = 0.6;
+var duty_min = 0.03;
 
 var xmin = 0.3;
 var ymin = 0.5;
@@ -15,6 +17,7 @@ var stdin = [0, 0];
 //Variables end
 
 b.pinMode(xSERVO, b.ANALOG_OUTPUT);
+b.pinMode(ySERVO, b.ANALOG_OUTPUT);
 updateDuty();
 
 var readline = require('readline');
@@ -23,19 +26,18 @@ var rl = readline.createInterface({
     output: process.stdout,
     terminal: false
 });
-
-
-
+rl.on('line', onLine);
 
 function updateDuty() {
     // compute and adjust duty_cycle based on
     // desired position in range 0..1
-    var duty_cycle = (xposition * 0.115) + xmin;
+    var duty_cycle = (xposition * 0.115) + duty_min;
     b.analogWrite(xSERVO, duty_cycle, 60);
+    var Yduty_cycle = (yposition * 0.115) + duty_min;
+    b.analogWrite(ySERVO, Yduty_cycle, 60);
 }
 
-rl.on('line', function(line) {
-
+function onLine(line) {
     console.log(line);
     stdin = line.split(" ");
     if (stdin[0] > 20) {
@@ -55,4 +57,4 @@ rl.on('line', function(line) {
     if (yposition < ymin) yposition = ymin;
     if (yposition > ymax) yposition = ymax;
     updateDuty();
-});
+}
