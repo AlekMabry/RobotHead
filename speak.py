1c6350748b9e35447f98dabf6076225e0045edf4
AlekMabry <AlekMabry24@gmail.com>
2015-08-26 11:41:14 -0400

    Added a loop and more commands

--- speak.py
+++ speak.py
@@ -1,10 +1,19 @@
 from subprocess import call
-
+import datetime
 
 def speak(user_input):
-    call(["flite", "-t", user_input)])
+    call(["flite", "-t", user_input])
 
-command = raw_input()
+while(1==1):
 
-if(command == "Where can I find your code?" or command == "Where can I find out more about you?"):
-    speak("Go to einsteinium studios dot com")
+    command = raw_input()
+
+    if(command == "Where can I find your code?" or command == "Where can I find out more about you?"):
+        speak("Go to einsteinium studios dot com")
+    
+    if(command == "What is the time?"):
+        now = datetime.datetime.now()
+        speak(now.hour+" "+now.minute)
+    
+    if(command == "What is your name?"):
+        speak("I am the Smart Alek Robot Head")
