10e06fd97fb511702eef0fa1a8e29d256a2c2345
AlekMabry <alekmabry24@gmail.com>
2018-08-05 14:28:40 +0530

    Moved old code to old_referenceCode directory

--- Blob Detection/Camera_Test.py
+++
@@ -1,9 +0,0 @@
-//Use this python script to test your Camera
-//Code is from dirty dish detector library (https://gist.github.com/nicknormal/347c18d0b27d45e33f73).
-
-import cv
-capture = cv.CaptureFromCAM(-1)
-cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, 960)
-cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, 720)
-im = cv.QueryFrame(capture)
-cv.SaveImage("/var/lib/cloud9/Camera_Test.jpg", im)
--- FaceDetection/input.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-a = 1
-
-while a == 1:
-	if sys.stdin.read() is not " ":
-		print "Detected"
